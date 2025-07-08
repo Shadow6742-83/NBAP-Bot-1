5a1b25e8b9ab088a57f6c9d68d7647ec310f499d
Rafael <rafa.xt@protonmail.com>
2025-07-08 15:13:23 -0300

    Update createNewSchool.py

--- createNewSchool.py
+++ createNewSchool.py
@@ -1,6 +1,6 @@
 # Tudo após "#" é um comentário
 
-# Esse script foi desenvolvido para criar itens, no Wikidata, para escolas brasileiras que ainda não existem
+# Esse código foi desenvolvido para criar itens, no Wikidata, para escolas brasileiras que ainda não existem
 # neste projeto. Utiliza, como fonte, o Censo Escolar de 2022. Para isso, segue três etapas:
 # 1) A partir de um arquivo contendo dados do Censo, obtém o Código INEP de uma escola;
 # 2) Realiza uma query (SPARQL) para conferir se a escola já existe no Wikidata;
@@ -12,14 +12,36 @@
 # Vamos trabalhar com pywikibot que é uma biblioteca, logo, precisamos importá-la
 import pywikibot
 
-# Etapa 1: obter o código INEP de um arquivo fonte
-# Desenvolver etapa 1 aqui...
+# Etapa 1: obter os dados da escola de um arquivo fonte
+
+
+
+
 
 
 # Etapa 2: realizar a consulta para verificar e existência (ou não) de um item
-# Desenvolver etapa 2 aqui...
+
+# Nesta variável, vamos armazenar nossa query
+consulta = "desenvolver query aqui..."
+
+# Para realizar a consulta, podemos utilizar essa função
+generator = pagegenerators.WikidataSPARQLPageGenerator(sparql_query, site=site)
+
+
+
 
 
 # Etapa 3: caso o item ainda não exista (conforme verificado na etapa 2), criá-lo
-# Desenvolver etapa 3 aqui...
+
+# Nesta variável (array), vamos armazenar as informações que gostaríamos de adicionar em nosso item
+dados = {
+    'labels': {
+        'en': 'Exemplo de rótulo em inglês',
+        'pt': 'Exemplo de rótulo em português',
+    },
+    'descriptions': {
+        'en': 'Exemplo de descrição em inglês',
+        'pt': 'Exemplo de descrição em português',
+    }
+}
 
